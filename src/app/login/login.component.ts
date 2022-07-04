13de18810230d2f8805e408fd2abc1820122a8d0
divya2398 <divya.platosys@gmail.com>
2022-07-04 12:18:38 +0530

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,22 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+ name=""
+ password=""
   users =[
     {
       Name : "divya",
-      Password:12345,
+      Password:"12345",
       verified:true
 
     },
     {
       Name : "kala",
-      Password:12345,
+      Password:"12345",
       verified:true
     },
     {
       Name : "venkat",
-      Password:12345,
-      verified:true
+      Password:"1234",
+      verified:false
     },
   
   ]
@@ -29,5 +32,14 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+ verify(){
+  for(let x of this.users) {
+   if(x.Name === this.name && x.Password === this.password){
+    alert("Welcome"+" "+x.Name)
+   }
+  
+  }
+ 
+ }
  
 }
